f09a86b5d95097d27590366e8282faa101fa9d74
Luke Bangs <bangsluke@gmail.com>
2023-08-30 21:35:07 +0100

    Styled header with new icon added

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,19 +1,28 @@
-import Image from "next/image";
+import ArrowBackSharpIcon from "@mui/icons-material/ArrowBackSharp";
+import MenuSharpIcon from "@mui/icons-material/MenuSharp";
+import TravelExploreIcon from "@mui/icons-material/TravelExplore";
+import { useRouter } from "next/router";
 import styles from "../styles/Home.module.css";
 
 // Define a nav bar component that holds the logo and the page name.
 export default function NavBar() {
+	const router = useRouter();
+
 	return (
 		<nav id='navbar' className={styles.navbar}>
-			<Image
-				src='/images/Logo.png' // Route of the image file
-				height={80} // Desired size with correct aspect ratio
-				width={80} // Desired size with correct aspect ratio
-				alt='Logo'
-				className={styles.logo}
-			/>
-
-			<h1>my travel journal.</h1>
+			<div className={styles.navbar_logoContainer}>
+				<TravelExploreIcon sx={{ fontSize: 50 }} />
+			</div>
+			<div className={styles.navbar_headerContainer}>
+				<h1>my travel journal.</h1>
+			</div>
+			<div className={styles.navbar_backContainer}>
+				{/* Add a back arrow to navigate the last page */}
+				<ArrowBackSharpIcon sx={{ fontSize: 50 }} onClick={() => router.back()} />
+			</div>
+			<div className={styles.navbar_menuContainer}>
+				<MenuSharpIcon sx={{ fontSize: 50 }} />
+			</div>
 		</nav>
 	);
 }
