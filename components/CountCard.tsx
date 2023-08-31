b29bf399776c2ef54f1e8ccbb1f12a7e2ed988f4
Luke Bangs <61457009+bangsluke@users.noreply.github.com>
2023-08-31 08:32:34 +0100

    Added hyperlinks to card counts

--- components/CountCard.tsx
+++ components/CountCard.tsx
@@ -1,16 +1,20 @@
+import { useRouter } from "next/router";
 import styles from "../styles/Travel.module.css";
 
 interface CountCardProp {
 	id: string;
 	cardTitle: string;
 	countValue: number | string;
+	pagePath: string;
 }
 
 // Define a count card component that holds a string title and a number.
 export default function CountCard(props: CountCardProp) {
-	const { id, cardTitle, countValue } = props; // Extract the props
+	const router = useRouter();
+
+	const { id, cardTitle, countValue, pagePath } = props; // Extract the props
 	return (
-		<div id={id} className={styles.countcard}>
+		<div id={id} className={styles.countcard} onClick={() => router.push({ pathname: pagePath })}>
 			<h3>{cardTitle}</h3>
 			<h4>{countValue}</h4>
 		</div>
