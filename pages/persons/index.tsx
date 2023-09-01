690a527f0a8f3b1ef00e58f77c77de5292a9b82f
Luke Bangs <bangsluke@gmail.com>
2023-09-01 08:30:26 +0100

    Removed unused back buttons

--- pages/persons/index.tsx
+++ pages/persons/index.tsx
@@ -1,9 +1,6 @@
-import { useRouter } from "next/router";
 import PersonsList from "../../components/Lists/PersonsList";
 
 export default function PersonsPage(props: any) {
-	const router = useRouter();
-
 	return (
 		<>
 			{/* Note: Layout wraps component in a main tag */}
@@ -11,21 +8,6 @@ export default function PersonsPage(props: any) {
 				<h1>Person Page</h1>
 
 				<PersonsList />
-
-				<div
-					style={{
-						backgroundColor: "blue",
-						color: "white",
-						margin: "3rem auto 0rem auto",
-						padding: "0.3rem 1.5rem",
-						lineHeight: "0.5",
-						borderRadius: "0.5rem",
-						cursor: "pointer",
-					}}
-					onClick={() => router.back()} // Go back to the last visited page
-				>
-					<h4>Click here to go back</h4>
-				</div>
 			</section>
 		</>
 	);
