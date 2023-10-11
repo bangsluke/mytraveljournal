892627aa628ca7075370afe2fd4759ae09ad90ec
Luke Bangs <61457009+bangsluke@users.noreply.github.com>
2023-10-11 16:52:49 +0100

    Initial creation of Map Chart

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
 import jsonData from "../backend/output.json"; // Adjust the path accordingly
 import CountCardSection from "../components/CountCardSection";
 import CitiesList from "../components/Lists/CitiesList";
 import CountryList from "../components/Lists/CountriesList";
 import HolidayList from "../components/Lists/HolidayList";
+import MapChart from "../components/MapChart";
 import MarkdownList from "../components/MarkdownList";
 import styles from "../styles/Home.module.css";
 
@@ -11,16 +11,11 @@ export default function Home(props: any) {
 	return (
 		<>
 			{/* Note: Layout wraps component in a main tag */}
-			<Image
-				src='/images/experiences/Nice.jpg' // Route of the image file
-				height={250} // Desired size with correct aspect ratio
-				width={330} // Desired size with correct aspect ratio
-				alt='Nice Photo'
-				priority
-			/>
 
 			<section>
 				<h1>My Travel Journal</h1>
+
+				<MapChart />
 
 				<CountCardSection />
 
