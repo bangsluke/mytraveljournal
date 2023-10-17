9cfc6f308410fadf4af7aa7d27bab222816a5f2a
Luke Bangs <bangsluke@gmail.com>
2023-10-17 11:26:25 +0100

    Added continent to sidebar

--- components/Sidebar/SidebarData.ts
+++ components/Sidebar/SidebarData.ts
@@ -8,6 +8,7 @@ export interface SidebarLink {
 // Define the data used for the sidebar.
 export const SidebarData: SidebarLink[] = [
 	{ id: "1", displayName: "Holidays", pagePath: "/holidays" },
-	{ id: "2", displayName: "Countries", pagePath: "/countries" },
-	{ id: "3", displayName: "People", pagePath: "/people" },
+	{ id: "2", displayName: "Continents", pagePath: "/continents" },
+	{ id: "3", displayName: "Countries", pagePath: "/countries" },
+	{ id: "4", displayName: "People", pagePath: "/people" },
 ];
