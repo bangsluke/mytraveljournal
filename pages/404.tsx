96cbb5f92ce0dca59fcd510e4409af801e86e21b
Luke Bangs <bangsluke@gmail.com>
2023-10-17 10:50:10 +0100

    Fixed 404 inline styling

--- pages/404.tsx
+++ pages/404.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import { useRouter } from "next/router";
 import Layout from "../components/Layouts/Layout";
 import Error404Image from "../public/images/404Image.webp";
+import styles from "../styles/Home.module.css";
 
 // https://nextjs.org/docs/advanced-features/custom-error-page
 export default function Custom404Page() {
@@ -9,17 +10,9 @@ export default function Custom404Page() {
 	return (
 		<Layout NavbarStyle='Opaque'>
 			<Image src={Error404Image} alt='Travel Site Error Logo' width='200' height='200' layout='fixed' loading='lazy' />
-			<h2 style={{ fontWeight: 600, fontSize: 18 }}>404 - Page Not Found</h2>
+			<h2 className={styles.ErrorPageHeader}>404 - Page Not Found</h2>
 			<div
-				style={{
-					backgroundColor: "blue",
-					color: "white",
-					margin: "3rem auto 0rem auto",
-					padding: "0.3rem 1.5rem",
-					lineHeight: "0.5",
-					borderRadius: "0.5rem",
-					cursor: "pointer",
-				}}
+				className={styles.ErrorMessageDiv}
 				onClick={() => router.back()} // Go back to the last visited page
 			>
 				<h4>Click here to go back</h4>
