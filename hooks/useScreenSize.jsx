e4a6bdcb8f3cfc920addc2f2ea8d711127565f2a
Luke Bangs <bangsluke@gmail.com>
2023-10-17 11:27:02 +0100

    Cleared console message from hook

--- hooks/useScreenSize.jsx
+++ hooks/useScreenSize.jsx
@@ -12,7 +12,7 @@ const useScreenSize = () => {
 		} else {
 			setScreenSize("desktop");
 		}
-		console.log("Screen size:", screenSize); // Log the screen size after resizing
+		// console.log("Screen size:", screenSize); // Log the screen size after resizing
 	};
 
 	useEffect(() => {
