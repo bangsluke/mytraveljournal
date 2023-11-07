b2ae1fa84d82a502c4016fff1fcc2956450aaa54
Luke Bangs <bangsluke@gmail.com>
2023-11-07 15:00:02 +0000

    Re-ordered index.js and upgraded @apollo/server

--- backend/graphql/index.js
+++ backend/graphql/index.js
@@ -5,14 +5,50 @@ import "dotenv/config.js"; // https://stackoverflow.com/a/65292523
 import neo4j from "neo4j-driver";
 // import customResolvers from "./customResolvers";
 
+// Display the NEO4J_CREDENTIALS nicely formatted in the console
+const NEO4J_CREDENTIALS = `{
+	uri: ${process.env.NEO4J_URI},
+	user: ${process.env.NEO4J_USER},
+	password: ${process.env.NEO4J_PW},
+}`;
+console.log("NEO4J_CREDENTIALS", NEO4J_CREDENTIALS);
+
+// Add a check to make sure the environment variables are indeed being loaded
+if (!process.env.NEO4J_URI || !process.env.NEO4J_USER || !process.env.NEO4J_PW) {
+	throw new Error("Neo4j environment variables not set");
+}
+
+// Connect to Neo4j
+const driver = neo4j.driver(
+	process.env.NEO4J_URI, // Use 127.0.0.1 instead of localhost - https://stackoverflow.com/a/73777897
+	neo4j.auth.basic(process.env.NEO4J_USER, process.env.NEO4J_PW),
+);
+
+// Ensure that the driver can actually connect to the database and that the connection is not closing prematurely for some reason
+driver
+	.verifyConnectivity()
+	.then(() => console.log("Successfully connected to Neo4j"))
+	.catch((error) => console.error("Could not connect to Neo4j", error));
+
+// Check the driver before creating the Apollo server
+if (!driver) {
+	throw new Error("Neo4j driver not initialized");
+}
+
 // Define custom resolvers
 const customResolvers = {
 	Query: {
 		personHolidayCount: async (_, { nodeId }, context) => {
-			// Inspect the context in your custom resolver
-			console.log("Context in resolver:", context); // Check what's inside the context
+			console.log("Context:", context);
 			if (!context.driver) {
+				console.error("Driver is undefined in context");
 				throw new Error("Driver not found in context");
+			}
+
+			// Inspect the context in your custom resolver
+			//console.log("Context in resolver:", context); // Check what's inside the context
+			if (!context.driver) {
+				// throw new Error("Driver not found in context");
 			}
 
 			// Implement the custom logic using the Neo4j driver
@@ -37,16 +73,17 @@ const customResolvers = {
 	},
 };
 
-const typeDefs = `#graphql
-    type Movie {
-        title: String
-        actors: [Actor!]! @relationship(type: "ACTED_IN", direction: IN)
-    }
+// type Movie {
+//     title: String
+//     actors: [Actor!]! @relationship(type: "ACTED_IN", direction: IN)
+// }
 
-    type Actor {
-        name: String
-        movies: [Movie!]! @relationship(type: "ACTED_IN", direction: OUT)
-    }
+// type Actor {
+//     name: String
+//     movies: [Movie!]! @relationship(type: "ACTED_IN", direction: OUT)
+// }
+
+const typeDefs = `#graphql
 
 	type Continent {
 		name: String!
@@ -90,36 +127,21 @@ const typeDefs = `#graphql
 	}
 
 	type Query {
-		personHolidayCount(nodeId: String!): PersonHolidayCount!
+		personHolidayCount(nodeId: String!): PersonHolidayCount
 	}
 
 	type PersonHolidayCount {
 		nodeId: String!
 		holidayCount: Int!
 	}
+
+	type Query {
+    	test: String
+  	}
 `;
 
 // Display the typeDefs nicely formatted in the console
 //console.log("typeDefs from backend/graphql/index.js", typeDefs);
-
-// Display the NEO4J_CREDENTIALS nicely formatted in the console
-const NEO4J_CREDENTIALS = `{
-	uri: ${process.env.NEO4J_URI},
-	user: ${process.env.NEO4J_USER},
-	password: ${process.env.NEO4J_PW},
-}`;
-console.log("NEO4J_CREDENTIALS", NEO4J_CREDENTIALS);
-
-// Add a check to make sure the environment variables are indeed being loaded
-if (!process.env.NEO4J_URI || !process.env.NEO4J_USER || !process.env.NEO4J_PW) {
-	throw new Error("Neo4j environment variables not set");
-}
-
-// Connect to Neo4j
-const driver = neo4j.driver(
-	process.env.NEO4J_URI, // Use 127.0.0.1 instead of localhost - https://stackoverflow.com/a/73777897
-	neo4j.auth.basic(process.env.NEO4J_USER, process.env.NEO4J_PW),
-);
 
 // Generate the initial schema
 const neoSchema = new Neo4jGraphQL({
@@ -131,25 +153,34 @@ const neoSchema = new Neo4jGraphQL({
 // Build the schema
 const schema = await neoSchema.getSchema(); // This now includes custom resolvers
 
-// Check the driver before creating the Apollo server
-if (!driver) {
-	throw new Error("Neo4j driver not initialized");
-}
-
 // Now neoSchema includes both the generated resolvers from your typeDefs as well as any custom resolvers you have defined.
+// const server = new ApolloServer({
+// 	schema,
+// 	context: ({ req }) => {
+// 		const context = {
+// 			req,
+// 			driver,
+// 		};
+// 		console.log("Complete context:", context); // Log the entire context
+// 		return context;
+// 	},
+// });
+
 const server = new ApolloServer({
-	schema,
-	context: ({ req }) => {
-		console.log("Driver in context:", driver); // This should not be undefined
-		return {
-			req,
-			driver,
-		};
+	typeDefs,
+	resolvers: {
+		Query: {
+			// Define a simple test resolver
+			test: () => "Test successful",
+		},
+	},
+	context: {
+		driver, // The Neo4j driver instance
 	},
 });
 
 const { url } = await startStandaloneServer(server, {
-	context: async ({ req }) => ({ req }), // Remove the context from here if it's causing the override
+	//context: async ({ req }) => ({ req }), // Remove the context from here if it's causing the override
 	listen: { port: 4000 },
 });
 
