36b473e0c25c6dab83cdb2e9fa780d3c1b45425c
Luke Bangs <bangsluke@gmail.com>
2023-12-24 15:25:29 +0000

    Updated loading component to be full screen

--- components/Loading/Loading.tsx
+++ components/Loading/Loading.tsx
@@ -4,7 +4,7 @@ import styles from "./Loading.module.css";
 // Define a sign in component can be used to help the user sign in.
 export default function Loading() {
 	return (
-		<div className={styles.AuthenticationHolder}>
+		<div className={styles.LoadingHolder}>
 			<Image src='/images/Logo.png' width={50} height={50} alt='My Travel Journal Logo' className={styles.rotatingLogo} />
 		</div>
 	);
