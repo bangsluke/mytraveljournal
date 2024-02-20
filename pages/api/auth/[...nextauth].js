9e78fc5f7790d2318462583cbfb813f090e20352
Luke Bangs <bangsluke@gmail.com>
2024-02-20 20:53:51 +0000

    Added commented out additional providers for later

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,4 +1,7 @@
 import NextAuth from "next-auth";
+// import AppleProvider from "next-auth/providers/apple";
+// import EmailProvider from "next-auth/providers/email";
+// import FacebookProvider from "next-auth/providers/facebook";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 
@@ -6,6 +9,14 @@ import GoogleProvider from "next-auth/providers/google";
 export const authOptions = {
 	// Configure one or more authentication providers
 	providers: [
+		// AppleProvider({
+		// 	clientId: process.env.APPLE_ID,
+		// 	clientSecret: process.env.APPLE_SECRET,
+		// }),
+		// FacebookProvider({
+		// 	clientId: process.env.FACEBOOK_ID,
+		// 	clientSecret: process.env.FACEBOOK_SECRET,
+		// }),
 		GithubProvider({
 			clientId: process.env.GITHUB_ID,
 			clientSecret: process.env.GITHUB_SECRET,
@@ -14,6 +25,11 @@ export const authOptions = {
 			clientId: process.env.GOOGLE_ID,
 			clientSecret: process.env.GOOGLE_SECRET,
 		}),
+		// Passwordless / email sign in
+		// EmailProvider({
+		// 	server: process.env.MAIL_SERVER,
+		// 	from: "NextAuth.js <no-reply@example.com>",
+		// }),
 		// ...add more providers here
 	],
 };
