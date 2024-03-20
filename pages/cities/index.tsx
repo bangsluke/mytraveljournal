3f03c7c344cdb4ce3a87abc146d91d67416051d8
Luke Bangs <bangsluke@gmail.com>
2024-03-20 14:31:51 +0000

    Renamed cities page

--- pages/cities/index.tsx
+++ pages/cities/index.tsx
@@ -5,18 +5,17 @@ import PageHeader from "../../components/PageHeader/PageHeader";
 import withAuth from "../../lib/withAuth";
 import styles from "../../styles/Home.module.css";
 
-function ContinentsPage(props: any, { session }: { session: Session }) {
+function CitiesPage(props: any, { session }: { session: Session }) {
 	return (
 		<Layout NavbarStyle='Opaque'>
 			{/* Note: Layout wraps component in a main tag */}
 
 			<section className={styles.section}>
 				<PageHeader PageHeaderTitle='Cities' />
-
 				<CitiesList />
 			</section>
 		</Layout>
 	);
 }
 
-export default withAuth(ContinentsPage);
+export default withAuth(CitiesPage);
