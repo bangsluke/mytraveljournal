5e1dc607caa1b0321be3679cd318ab4edc0cc51e
Luke Bangs <bangsluke@gmail.com>
2024-04-07 19:19:48 +0100

    Added sharp and removed DevMode from constants

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,25 +1,23 @@
 import { ApolloClient, ApolloProvider, InMemoryCache, gql } from "@apollo/client";
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
-import Constants from "../constants/constants";
 import LogS from "../services/LogS";
 import "../styles/globals.css";
 
-// This App component is the top-level component which will be common across all the different pages.
-// You can use this App component to keep state when navigating between pages, for example.
+// This App component is the top-level component which will be common across all the different pages. You can use this App component to keep state when navigating between pages, for example.
 
-// Pull in and display the dev mode if it is on
-const { DevMode } = Constants;
-if (DevMode) {
-	LogS.warn("DevMode is on - set in constants.ts");
+// Get the mode (development or production) from the environment
+const runMode = process.env.NODE_ENV;
+LogS.log("Site running in mode: ", runMode);
+if (runMode === "development") {
+	LogS.warn("Site running in development");
 }
 
 // Define dark mode
-const darkMode = false;
-// TODO: Do something with this?
+//const darkMode = false; // TODO: Do something with this?
 
 // Note: All console logs will be printed in the browser console
-if (DevMode) {
+if (runMode === "development") {
 	LogS.log("NEXT_PUBLIC_APP_BACKEND_URL", process.env.NEXT_PUBLIC_APP_BACKEND_URL);
 }
 
@@ -29,25 +27,31 @@ const client = new ApolloClient({
 	cache: new InMemoryCache(),
 });
 
-// const client = ...
-client
-	.query({
-		query: gql`
-			query GetLocations {
-				cities {
-					name
-					nodeId
-					capital
-					timesVisited
+if (runMode === "development") {
+	LogS.log("Testing GraphQL connection in _app.tsx");
+	client
+		.query({
+			query: gql`
+				query GetLocations {
+					cities {
+						name
+						nodeId
+						capital
+						linkedHolidays {
+							nodeId
+							name
+						}
+					}
 				}
-			}
-		`,
-	})
-	.then((result) => {
-		if (DevMode) {
-			LogS.log("Test connection in _app.tsx", result);
-		}
-	});
+			`,
+		})
+		.then((result) => {
+			LogS.log("Tested GraphQL connection successfully in _app.tsx");
+		})
+		.catch((error) => {
+			LogS.error("Error in GraphQL test connection in _app.tsx", error);
+		});
+}
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
 	return (
