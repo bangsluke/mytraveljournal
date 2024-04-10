cb1dfb5ec7cdace1a6ccac3ae48f7d1032b2571b
Luke Bangs <bangsluke@gmail.com>
2024-04-10 20:10:01 +0100

    Added Manitine button component across the app

--- components/Sidebar/Sidebar.tsx
+++ components/Sidebar/Sidebar.tsx
@@ -5,6 +5,7 @@ import { useRouter } from "next/router";
 import React from "react";
 import DarkModeToggle from "../../components/DarkModeToggle/DarkModeToggle";
 import useScreenSize from "../../hooks/useScreenSize";
+import { ButtonComponent } from "../Button/Button";
 import styles from "./Sidebar.module.css";
 import { SidebarData } from "./SidebarData";
 import SidebarItem from "./SidebarItem";
@@ -55,9 +56,6 @@ const Sidebar: React.FC<SidebarProps> = ({ sidebarOpen, toggleSidebar }) => {
 				{/* Dynamically decide on if to show the close sidebar button */}
 				{sidebarStyle == "dynamic" && (
 					<div className={styles.sidebarCloseButtonContainer}>
-						<button className={styles.toggleButton} onClick={toggleSidebar}>
-							Close Sidebar
-						</button>
 						<CloseButton onClick={toggleSidebar} size='lg' aria-label='Close sidebar' />
 					</div>
 				)}
@@ -75,19 +73,17 @@ const Sidebar: React.FC<SidebarProps> = ({ sidebarOpen, toggleSidebar }) => {
 					</ul>
 				</div>
 				{/* Provide details to the user on their login email and offer a sign out button */}
-				<div className={styles.sidebarLoginContent}>
+				<Group justify='center' className={styles.sidebarLoginContent}>
 					{/* Display either a sign in or sign out button based on the session state */}
 					{session ? (
 						<>
 							<p>Signed in as {signedInEmail}</p>
 							{/* TODO: Style email underlined */}
-							<button onClick={() => signOut()}>Sign out</button>
+							<ButtonComponent Text='Sign out' onClick={() => signOut()} />
 						</>
 					) : (
-						<button onClick={() => signIn()}>Sign in</button>
+						<ButtonComponent Text='Sign in' onClick={() => signIn()} />
 					)}
-				</div>
-				<Group justify='center'>
 					<DarkModeToggle></DarkModeToggle>
 				</Group>
 			</nav>
