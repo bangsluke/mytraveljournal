a5e1ed089c655b841f062b0d52e92195841c10e8
Luke Bangs <bangsluke@gmail.com>
2024-04-25 17:15:03 +0100

    Part fix of Cities List page

--- components/Lists/CitiesList.tsx
+++ components/Lists/CitiesList.tsx
@@ -1,6 +1,9 @@
 import { useQuery } from "@apollo/client";
+import { ActionIcon, Anchor, ScrollArea, Table, Text, Tooltip, rem } from "@mantine/core";
+import ArrowForwardSharpIcon from "@mui/icons-material/ArrowForwardSharp";
 import { useRouter } from "next/router";
-import { City, GetCitiesListDocument } from "../../graphql/__generated__/graphql";
+import Constants from "../../constants/constants";
+import { GetCitiesListDocument } from "../../graphql/__generated__/graphql";
 import LogS from "../../services/LogS";
 import Loading from "../Loading/Loading";
 import Toast from "../Toast/Toast";
@@ -17,8 +20,7 @@ export default function CitiesList() {
 		LogS.error("useQuery(GetCitiesListDocument) GraphQL Error: ", error.message);
 		return <Toast message={"useQuery(GetCitiesListDocument) GraphQL Error: " + error.message} duration={5} />;
 	}
-
-	// LogS.log("CitiesList: cities data: ", data);
+	LogS.log("CitiesList: cities data: ", data);
 
 	// Filter out cities with no holidays and then sort by the length of timesVisited
 	//const sortedAndFilteredCities = data.cities;
@@ -28,15 +30,77 @@ export default function CitiesList() {
 
 	LogS.log("CitiesList: Sorted and filtered city data: ", sortedAndFilteredCities);
 
+	// Get the height of the scrollable area
+	const windowHeight = window.innerHeight;
+	const scrollHeight = windowHeight - Constants.headerHeight;
+
+	// Map the sorted and filtered cities to create the table rows
+	const rows = sortedAndFilteredCities?.map((city: any) => (
+		<Table.Tr key={city.nodeId} className={styles.rowHighlight}>
+			<Table.Td>
+				<Anchor
+					component='button'
+					size='sm'
+					fw={500}
+					onClick={() => router.push({ pathname: `/cities/${city.nodeId}` })}
+					className={styles.leftAlign}>
+					{city.name}
+				</Anchor>
+			</Table.Td>
+			<Table.Td>
+				<Text fz='md' fw={500}>
+					{city.visitedCount}
+				</Text>
+			</Table.Td>
+			<Table.Td>
+				<Anchor
+					component='button'
+					size='sm'
+					onClick={() => router.push({ pathname: `/holidays/${city.lastHoliday.nodeId}` })}
+					className={styles.leftAlign}>
+					{city.lastHoliday.name}
+				</Anchor>
+			</Table.Td>
+			<Table.Td>
+				<ActionIcon variant='subtle' color='gray'>
+					<ArrowForwardSharpIcon
+						style={{ width: rem(16), height: rem(16) }}
+						onClick={() => router.push({ pathname: `/cities/${city.nodeId}` })}
+					/>
+				</ActionIcon>
+			</Table.Td>
+		</Table.Tr>
+	));
+
 	return (
-		<div className={styles.dataList}>
-			<ul>
-				{sortedAndFilteredCities.map(({ name, nodeId }: City) => (
-					<li key={nodeId} className={styles.clickableListItem} onClick={() => router.push({ pathname: `/cities/${nodeId}` })}>
-						<h4>{name}</h4>
-					</li>
-				))}
-			</ul>
-		</div>
+		<ScrollArea h={scrollHeight} className={styles.dataList}>
+			<Table.ScrollContainer minWidth={300}>
+				<Table verticalSpacing='sm'>
+					<Table.Thead>
+						<Table.Tr>
+							<Table.Th>Name</Table.Th>
+							<Tooltip label='Visited Count' withArrow>
+								<Table.Th>Count</Table.Th>
+							</Tooltip>
+							<Table.Th>Last Holiday</Table.Th>
+							<Table.Th />
+						</Table.Tr>
+					</Table.Thead>
+					<Table.Tbody>{rows}</Table.Tbody>
+				</Table>
+			</Table.ScrollContainer>
+		</ScrollArea>
 	);
+
+	// return (
+	// 	<div className={styles.dataList}>
+	// 		<ul>
+	// 			{sortedAndFilteredCities.map(({ name, nodeId }: City) => (
+	// 				<li key={nodeId} className={styles.clickableListItem} onClick={() => router.push({ pathname: `/cities/${nodeId}` })}>
+	// 					<h4>{name}</h4>
+	// 				</li>
+	// 			))}
+	// 		</ul>
+	// 	</div>
+	// );
 }
